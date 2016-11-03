1513524dc9388c5378e2f9c06580dead1734a447
ruan.sheng <ruan.sheng@immomo.com>
2016-11-03 13:51:39 +0800

    websocket

--- server.php
+++ server.php
@@ -1,36 +1,26 @@
 <?php
 date_default_timezone_set('PRC');
-// Run from command prompt > php -q chatbot.demo.php
-include "WebSocket.php";
-// Extended basic WebSocket as ChatBot
-class ChatBot extends WebSocket{
-	function process($user,$msg){
 
-		if (isset($user->first)) {
-			$this->send($user->socket,'');
-			$user->first = true;
-		}
+//创建websocket服务器对象，监听0.0.0.0:9502端口
+$ws = new swoole_websocket_server("0.0.0.0", 4000);
 
-		$this->say("< ".$msg);
-		switch($msg){
-			case "hello" : $this->send($user->socket,"hello human");                       break;
-			case "hi"    : $this->send($user->socket,"zup human");                         break;
-			case "name"  : $this->send($user->socket,"my name is Multivac, silly I know"); break;
-			case "age"   : $this->send($user->socket,"I am older than time itself");       break;
-			case "date"  : $this->send($user->socket,"today is ".date("Y.m.d"));           break;
-			case "time"  : $this->send($user->socket,"server time is ".date("H:i:s"));     break;
-			case "thanks": $this->send($user->socket,"you're welcome");                    break;
-			case "bye"   : $this->send($user->socket,"bye");                               break;
-			//default      : $this->send($user->socket,$msg." not understood");              break;
-			default      : $this->sendAll($user, $msg);              break;
-		}
-	}
-	function sendAll($currentUser, $msg){
-		$usersList = $this->users;
-		foreach ($usersList as $user){
-			if ($user !== $currentUser) // 自己发送的消息就不再接收一次了
-				$this->send($user->socket, $msg);
-		}
-	}
-}
-$master = new ChatBot("localhost",4000);
+//监听WebSocket连接打开事件
+$ws->on('open', function ($ws, $request) {
+	echo "server: handshake success with fd{$request->fd}".PHP_EOL;
+});
+
+//监听WebSocket消息事件
+$ws->on('message', function ($ws, $frame) {
+	$msg =  'from'.$frame->fd.":{$frame->data}\n";
+	echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}".PHP_EOL;
+	$ws->push($frame->fd, "this is server");
+});
+
+//监听WebSocket连接关闭事件
+$ws->on('close', function ($ws, $fd) {
+	echo "client-{$fd} is closed\n";
+});
+
+$ws->start();
+
+// /usr/local/php/bin/php -c /usr/local/php/etc/php.ini server.php
